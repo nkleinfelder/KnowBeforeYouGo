64626b1bea8b41305e05a5adf4671b8545b4fd9b
Ronny <nick.kleinfelder@rwth-aachen.de>
2025-12-06 18:56:19 +0100

    feat: added and tested api endpoint

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getRecommendation() {
+  const res = await fetch("http://localhost:3000/api/recommendation");
+  return res.json();
+}
+
+export default async function Home() {
+  const recommendation = await getRecommendation();
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
@@ -14,7 +20,8 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Know Before You Go.
+            {recommendation.message}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
