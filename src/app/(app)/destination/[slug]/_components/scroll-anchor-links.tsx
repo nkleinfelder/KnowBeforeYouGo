d6b59bb4b24663f1b544f16ff6470de595810383
SimonBiel0301 <me@simonbiel.com>
2025-12-08 19:50:07 +0100

    feat(country-details): add keyboard support for scroll-anchor-links

--- src/app/(app)/destination/[slug]/_components/scroll-anchor-links.tsx
+++ src/app/(app)/destination/[slug]/_components/scroll-anchor-links.tsx
@@ -1,6 +1,7 @@
 import { BlankScrollButton } from "@/src/components/scroll-button";
 import { cn } from "@/src/lib/utils";
 import { LucideIcon } from "lucide-react";
+import * as Navigation from "@radix-ui/react-navigation-menu";
 
 export function ScrollAnchorLinks({
   sections,
@@ -11,29 +12,38 @@ export function ScrollAnchorLinks({
   }[];
 }) {
   return (
-    <aside className="sticky top-6 col-start-4 col-end-5 row-start-2 row-end-3 h-12 justify-self-end">
-      <ol className="flex h-[min(calc(100dvh-2*var(--spacing)*6),35rem)] w-2 flex-col items-center justify-between bg-stone-200 shadow-lg">
-        {sections.map((section) => (
-          <li key={section.id}>
-            <AnchorLink Icon={section.Icon} id={section.id} />
-          </li>
-        ))}
-      </ol>
-    </aside>
+    <Navigation.Root asChild orientation="vertical">
+      <aside className="sticky top-6 col-start-4 col-end-5 row-start-2 row-end-3 h-12 justify-self-end">
+        <Navigation.List className="relative z-0 flex h-[min(calc(100dvh-2*var(--spacing)*6),35rem)] flex-col items-center justify-between">
+          {sections.map((section) => (
+            <Navigation.Item key={section.id}>
+              <AnchorLink Icon={section.Icon} id={section.id} />
+            </Navigation.Item>
+          ))}
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-auto -z-10 h-full w-2 bg-stone-200 shadow-lg"
+          />
+        </Navigation.List>
+      </aside>
+    </Navigation.Root>
   );
 }
 
 function AnchorLink({ Icon, id }: { Icon: LucideIcon; id: string }) {
   return (
-    <BlankScrollButton
-      scrollToId={id}
-      className={cn(
-        "flex cursor-pointer items-center justify-center rounded-full bg-stone-50 p-3 text-stone-700 shadow-sm transition-all duration-200 ease-out",
-        "focus-within:scale-115 hover:scale-115",
-        "focus-within:bg-violet-200 hover:bg-violet-200",
-      )}
-    >
-      <Icon className="size-5" />
-    </BlankScrollButton>
+    <Navigation.Link asChild>
+      <BlankScrollButton
+        scrollToId={id}
+        className={cn(
+          "flex cursor-pointer items-center justify-center rounded-full bg-stone-50 p-3 text-stone-700 shadow-sm transition-all duration-200 ease-out",
+          "hover:scale-115 focus-visible:scale-115",
+          "hover:bg-violet-200 focus-visible:bg-violet-200",
+          "focus-visible:ring-2 focus-visible:ring-primary focus-visible:outline-none",
+        )}
+      >
+        <Icon className="size-5" />
+      </BlankScrollButton>
+    </Navigation.Link>
   );
 }
